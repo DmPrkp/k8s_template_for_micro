06adf4c61cc04e20a1803700c269e64956d519d0
DmPrkp <f89522795153@gmail.com>
2021-11-08 22:14:18 +0300

    error handler in process...

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import 'express-async-errors';
+// import 'express-async-errors' from '';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
@@ -24,5 +25,5 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
+  console.log('Listening on port 3000!!!');
 });
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
