4d041a66f757dfe6e24bdf718c1690aa1baf08b5
DmPrkp <f89522795153@gmail.com>
2021-11-12 20:07:41 +0300

    added experess-async-errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-// import 'express-async-errors' from '';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
