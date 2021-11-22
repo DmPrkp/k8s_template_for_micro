bd1f64275c265cb3e0fd0b86d650efea868c266b
DmPrkp <f89522795153@gmail.com>
2021-11-22 22:45:52 +0300

    auth done

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,6 +3,9 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
+  if (req.session) {
+    req.session = null
+  }
   res.send("Hi there! signOut");
 });
 
